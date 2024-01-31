69eca4c8c3316b806a48a6f5fe8d24dae0694e59
David Gaither <gaither@protonmail.com>
2024-01-31 18:53:43 -0500

    Linear and global linear functions

--- linearSearch.js
+++ linearSearch.js
@@ -1,9 +1,21 @@
 function linearSearch(searchTerm, arr) {
-  return undefined;
+  let output = undefined
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == searchTerm) {
+      output = i
+      return output
+    }
+  }
 }
 
 function globalLinearSearch(searchTerm, arr) {
-  return [];
+  let indices = []
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == searchTerm) {
+      indices.push(i)
+    }
+  }
+  return indices
 }
 
 module.exports = { linearSearch, globalLinearSearch };
